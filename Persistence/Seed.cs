86556d67986a3a62f5622e878507804df3a1beac
Michal Fiala <michfiala.it@gmail.com>
2021-07-24 01:15:22 +0200

    Changing seed of first element

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -15,119 +15,119 @@ namespace Persistence
 			{
                 new Product
                 {
-                    Name = "HP",
+                    Name = "Produkt - 1",
                     ImgUri = "laptop1.jpg",
                     Price = 14999.9m,
-                    Description = null
+                    Description = "Test description"
                 },
                 new Product
                 {
-                    Name = "Dell",
+                    Name = "Produkt - 2",
                     ImgUri = "laptop2.jpg",
                     Price = 28999.0m,
                     Description = "Dell laptop"
                 },
                 new Product
                 {
-                    Name = "Keyboard",
+                    Name = "Produkt - 3",
                     ImgUri = "keyboard.jpg",
                     Price = 1400.0m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Mouse",
+                    Name = "Produkt - 4",
                     ImgUri = "mouse.jpg",
                     Price = 400m,
                     Description = "Verticall mouse"
                 },
                 new Product
                 {
-                    Name = "Charger",
+                    Name = "Produkt - 5",
                     ImgUri = "charger.jpg",
                     Price = 580.666666m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Laptop - 1",
+                    Name = "Produkt - 6",
                     ImgUri = "laptop2.jpg",
                     Price = 580.666666m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Mouse - 1",
+                    Name = "Produkt - 7",
                     ImgUri = "mouse.jpg",
                     Price = 800m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Laptop - 2",
+                    Name = "Produkt - 8",
                     ImgUri = "laptop1.jpg",
                     Price = 7000m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Charger - 1",
+                    Name = "Produkt - 9",
                     ImgUri = "charger.jpg",
                     Price = 336m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Laptop - 3",
+                    Name = "Produkt - 10",
                     ImgUri = "laptop2.jpg",
                     Price = 58000m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Keyboard - 1",
+                    Name = "Produkt - 11",
                     ImgUri = "keyboard.jpg",
                     Price = 780m,
                     Description = null
                 },
-                                new Product
+                new Product
                 {
-                    Name = "Laptop - 4",
+                    Name = "Produkt - 12",
                     ImgUri = "laptop2.jpg",
                     Price = 580.666666m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Mouse - 2",
+                    Name = "Produkt - 13",
                     ImgUri = "mouse.jpg",
                     Price = 800m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Laptop - 5",
+                    Name = "Produkt - 14",
                     ImgUri = "laptop1.jpg",
                     Price = 7000m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Charger - 3",
+                    Name = "Produkt - 15",
                     ImgUri = "charger.jpg",
                     Price = 336m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Laptop - 6",
+                    Name = "Produkt - 16",
                     ImgUri = "laptop2.jpg",
                     Price = 58000m,
                     Description = null
                 },
                 new Product
                 {
-                    Name = "Keyboard - 3",
+                    Name = "Produkt - 17",
                     ImgUri = "keyboard.jpg",
                     Price = 780m,
                     Description = null
